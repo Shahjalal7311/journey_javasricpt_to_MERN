de664894e00bc06d1fa1e70be84ac65af494722d
shahjalal <shah.jalal@reivo.jp.com>
2022-08-03 10:25:45 +0600

    practice and little description different function

---
+++ function_declaration.js
@@ -0,0 +1,11 @@
+// Named function Declaration and call
+function test(){ // this line is function Declaration
+  console.log('check name function');
+}
+// test();
+
+// Variable function Declaration
+const date_fun = function(){
+  console.log( Date.now());
+}
+date_fun();
